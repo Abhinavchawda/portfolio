026857f6b7e2e26773a2ffb5269345c1957fbb25
Abhinav <abhinavchawda23@gmail.com>
2025-08-30 22:27:17 +0530

    Solve the dependency issues in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Home from './components/Home';
 import Education from './components/Education';
 import ProjectSlider from './components/Project';
 import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Achievements from './components/Achievements';
 import Footer from './components/Footer';
 
 export default function App() {
-
   return (
     <div className="overflow-hidden bg-gradient-to-br from-gray-900 via-black to-gray-800">
       <Navbar />
       <Home />
+      <div className="border-y border-blue-500"><Experience /></div>
       <div className="border-y border-blue-500"><Education /></div>
+      <div className="border-b border-blue-500"><Skills /></div>
       <div className="border-b border-blue-500"><ProjectSlider /></div>
-      <Skills />
+      <div className="border-b border-blue-500"><Achievements /></div>
       <Footer />
-    </div >
-  )
+    </div>
+  );
 };
